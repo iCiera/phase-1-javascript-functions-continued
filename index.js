3e429b3987673a6ab3279c37b07ff0731bc285b1
ciera <cieranicole.m15@gmail.com>
2022-03-23 21:41:28 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+   }
+
+   function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+  }
+
+  function wrapAdjective(param2 = "*"){
+    return function (param = "special") {
+      return `You are ${param2}${param}${param2}!`
+    }
+  }
